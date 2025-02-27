ed7b7748bd3bd1955cc512229ff8e00e4ccc3367
coll12345 <152050891+coll12345@users.noreply.github.com>
2025-02-27 17:00:10 +0530

    Create config.py

---
+++ Main/config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = "24792758"
+API_HASH = "97b7af6fcde9a4236057ff1eb3093753"
+BOT_TOKEN = "7830703443:AAGj54NfH-jWlL68fyJdbDPC44_FBTlneI4"
+MONGO_URI = "mongodb+srv://Dileep:Dileep@cluster0.gejzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
